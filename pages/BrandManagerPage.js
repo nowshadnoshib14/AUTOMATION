c5255d8a9cce043055db197f4b3ab35c7b7ff98f
nowshadnoshib14 <168620563+nowshadnoshib14@users.noreply.github.com>
2026-01-15 21:48:28 +0600

    Implement BrandManagerPage class for brand management

---
+++ pages/BrandManagerPage.js
@@ -0,0 +1,29 @@
+class BrandManagerPage {
+    constructor(page) {
+        this.page = page;
+        this.headerTitle = page.getByText('Brand Manager', { exact: true });
+        this.descriptionText = page.getByText('Manage your restaurant brand(');
+        this.dishioLogo = page.getByRole('img').first();
+        this.sidebarPersonalInfo = page.locator('.relative.flex.w-full.flex-row.items-center.space-x-\\[10px\\].whitespace-nowrap.rounded-lg.p-4.text-\\[\\#464A58\\]').first();
+        this.sidebarSecurity = page.locator('div:nth-child(3) > .group\\/menu-item > div > .flex.w-full.cursor-pointer > .relative');
+        this.sidebarPreferences = page.locator('div:nth-child(4) > .group\\/menu-item > div > .flex.w-full.cursor-pointer > .relative');
+        this.sidebarToggleExpand = page.locator('.cursor-pointer.\\!text-\\[\\#B3B3B3\\]');
+        this.sidebarToggleCollapse = page.getByRole('img').nth(5);
+        this.accountIcon = page.getByRole('button').filter({ hasText: /^$/ });
+        this.myAccountOption = page.getByRole('button', { name: 'My Account' });
+        this.learningOption = page.getByRole('link', { name: 'Learning' });
+        this.languageOption = page.getByText('Language');
+        this.logoutOption = page.getByRole('button', { name: 'Log out' });
+        this.brandSearch = page.getByRole('textbox', { name: 'Search' });
+        //language
+        this.eng = page.getByText('English');
+        this.es = page.getByText('Español');
+        this.por = page.getByText('Português');
+        this.fra = page.getByText('Français');
+
+    }
+    async openAccountMenu() {
+        await this.accountIcon.click;
+    }
+}
+module.exports = { BrandManagerPage };
